67801bb491ffcdd24a5c918eba688af61cf82c28
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2020-08-06 21:49:25 -0500

    Add exercise1-1.c

---
+++ Chapter-01/exercise1-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * Exercise 1-1. Run the "hello, world" program on your system.
+ * Experiment with leaving parts of the program, to see what error
+ * messages you get.
+ */
+
+int main()
+{
+	printf("hello, world");
+	return 0;
+}
+
