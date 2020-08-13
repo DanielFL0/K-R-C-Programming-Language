c1cc0b04c4219d03e44ff439c956527d7a8e0a5b
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2020-08-13 14:19:30 -0500

    Add exercise1-14.c

--- Chapter-01/exercise1-14.c
+++ Chapter-01/exercise1-14.c
@@ -5,9 +5,25 @@
  * of different characters in its input.
  */
 
+#define TOTALASCII 256
+
 int main()
 {
+    int i, c, j;
+    int ascii[TOTALASCII];
 
+    for (i = 0; i < TOTALASCII; ++i)
+        ascii[i] = 0;
+
+    while ((c = getchar()) != EOF)
+        ++ascii[c];
     
+    for (i = 0; i < TOTALASCII; ++i) {
+        printf("\n%d\t", i);
+        putchar(i);
+        putchar('\t');
+        for (j = 0; j < ascii[i]; ++j)
+            putchar('*');
+    }
     return 0;
 }
