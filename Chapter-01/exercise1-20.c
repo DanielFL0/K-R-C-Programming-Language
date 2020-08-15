d83f514a07476eb7af91c0d3f71ac7c7ecdab262
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2020-08-15 16:02:12 -0500

    Add exercise1-20.c

---
+++ Chapter-01/exercise1-20.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * Exercise 1-20. Write a program detab that replaces tabs in the
+ * input with the proper number of blanks to space to the next tab
+ * stop. Assume a fixed set of tab stops, say every n columns.
+ * Should n be a variable or a single blank would suffice to
+ * reach a tab stop, which should be given preference?
+ */
+
+#define TABWIDTH 8
+
+int main()
+{
+
+    return 0;
+}
