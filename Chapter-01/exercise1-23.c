faf25b540ecf5ed58b74e903189c3b77cf173144
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2020-08-18 20:17:29 -0500

    Add exercise1-23.c

---
+++ Chapter-01/exercise1-23.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * Exercise 1-23. Write a program to remove all comments from a C program.
+ * Don't forget to handle qouted strings and charecter constants
+ * properly. C comments do not nest.
+ */
+
+int main()
+{
+
+    return 0;
+}
